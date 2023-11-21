c81e15fa2b757564264c72638d3c3ac9e027b996
Nguyen Khanh <nguyenkhanh28031999@gmail.com>
2023-11-21 20:13:01 +0700

    Completed Lesson-2

--- routes/teachers.js
+++ routes/teachers.js
@@ -4,10 +4,27 @@ const DATA = require('../mock/teachers');
 
 const teacherRouter = express.Router();
 
+
+
 teacherRouter.get('/', (req,res) => {
-    res.json(DATA);
+    const {from,to} = req.query;
+    if (from && to) {
+        const newTeachers = DATA.Teachers.filter((teacher) => teacher.age >= from && teacher.age <= to);
+    res.json(newTeachers)    
+    }else {
+        res.json(DATA);
+    }
+    
 });
 
+teacherRouter.get('/:id', (req,res) => {
+    const teacher = DATA.Teachers.find(teacher => teacher.id === parseInt(req.params.id));
+    res.json(teacher)
+});
+
+//Array method : find : Tìm đối tượng teacher có id tương ứng
+//6 : Querry API : Khi truy cập địa chỉ ... from 18 ... to 40 ... 
+//sử dụng array method : filter
 
 module.exports = {teacherRouter};
 
